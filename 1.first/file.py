8213f4082d1b6164062a73a4a50115208155a102
Dukeson Ehigboria O <studysampul@gmail.com>
2023-03-09 14:27:38 +0100

    This is a simple project

--- 1.first/file.py
+++ 1.first/file.py
@@ -1,3 +1,9 @@
-print("This is really the first python program that I am designing on this workstation.")
+# First program written with Python programming language.
+
+print("This is really the first python program that I am " + \
+      "designing on this workstation.")
+
 val = int(input("How many programs do you wish to write on this workstation?\t"))
-print("Thank you for this response. You will be writing more than {} programs on this workstation.".format(val))
+
+print("Thank you for this response. You will be writing more than {} ".format(val) + \
+      "programs on this workstation.")
